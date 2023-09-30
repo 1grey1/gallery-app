78fb932df9631343d9c88959d2f1b91d4a71e329
1grey1 <125741721+1grey1@users.noreply.github.com>
2023-09-30 16:11:25 +0300

    Add files via upload

--- client/src/comment-list.js
+++ client/src/comment-list.js
@@ -1,25 +1,56 @@
+const totalCommentCountElement = document.querySelector('.comments-count');
+const renderedCommentCountElement = document.querySelector('.comments-count--rendered');
 const showMoreButtonElement = document.querySelector('.social__comments-loader');
 const commentListElement = document.querySelector('.social__comments');
 const commentTemlate = document.getElementById('comment')
     .content
     .querySelector('.social__comment');
 
-const renderCommentList = (comments) => {
-    commentListElement.innerHTML = '';
+const COMMENT_COUNT_PER_STEP = 5;
 
-    if (comments.length <= 5) {
-        showMoreButtonElement.classList.add('hidden');
-    } else {
-        showMoreButtonElement.classList.remove('hidden');
+const setLoaderClick = function (comments) {
+    if (this.onLoaderClick !== undefined) {
+        showMoreButtonElement.removeEventListener('click', this.onLoaderClick);
     }
 
-    for (const comment of comments) {
+    this.onLoaderClick = () => {
+        renderComments(comments, renderedCommentCount, renderedCommentCount + COMMENT_COUNT_PER_STEP);
+        renderedCommentCount += COMMENT_COUNT_PER_STEP;
+        if (renderedCommentCount >= comments.length) {
+            showMoreButtonElement.classList.add('hidden');
+        }
+    };
+
+    let renderedCommentCount = 5;
+    showMoreButtonElement.addEventListener('click', this.onLoaderClick);
+}
+
+const renderComments = (comments, from, to) => {
+    for (const comment of comments.slice(from, to)) {
         const commentElement = commentTemlate.cloneNode(true);
         commentElement.querySelector('.social__text').textContent = comment.message;
         commentElement.querySelector('.social__picture').setAttribute('src', comment.user.avatar);
         commentListElement.append(commentElement);
+        renderedCommentCountElement.textContent = String((+renderedCommentCountElement.textContent) + 1);
     }
 };
 
+const renderCommentList = (comments) => {
+    commentListElement.innerHTML = '';
+
+    const to = Math.min(comments.length, COMMENT_COUNT_PER_STEP);
+
+    renderComments(comments, 0, to);
+    totalCommentCountElement.textContent = String(comments.length);
+    renderedCommentCountElement.textContent = String(to);
+
+    if (comments.length > COMMENT_COUNT_PER_STEP) {
+        showMoreButtonElement.classList.remove('hidden');
+    } else {
+        showMoreButtonElement.classList.add('hidden');
+    }
+
+    setLoaderClick.call(renderCommentList, comments);
+};
 
 export {renderCommentList};
--- client/src/zoom.js
+++ client/src/zoom.js
@@ -1,7 +1,25 @@
+const comingButtonElement = document.querySelector('.scale__control--bigger');
+const withdrawaButtonElement = document.querySelector('.scale__control--smaller');
 const scaleControlValueElement = document.querySelector('.scale__control--value');
+let scaleControNumberElement = scaleControlValueElement.value = '100'.replace('%', '');
 
 const onScaleControlClick = () => {
-    console.log(scaleControlValueElement);
+    comingButtonElement.addEventListener('click', comingPicnehe);
+    withdrawaButtonElement.addEventListener('click', withdrawalPicnehe);
+};
+
+const comingPicnehe = () => {
+    if (Number(scaleControNumberElement) < 100){
+        scaleControNumberElement = Number(scaleControNumberElement) + 25;
+    }
+    scaleControlValueElement.value = '100%'.replace('100', String(scaleControNumberElement));
+};
+
+const withdrawalPicnehe = () => {
+    if (Number(scaleControNumberElement) > 0){
+        scaleControNumberElement = Number(scaleControNumberElement) - 25;
+    }
+    scaleControlValueElement.value = '100%'.replace('100', String(scaleControNumberElement));
 };
 
 export {onScaleControlClick};
